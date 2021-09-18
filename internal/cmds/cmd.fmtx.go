3ea674c2f6703daa2fc265b4ed2beb97ff81c3ff
darmiel <71837281+darmiel@users.noreply.github.com>
2021-09-18 19:10:18 +0200

    feat: added flag `remove-empty-lines` and `fast-fail`, feat: implemented progressbar feat: implements #1

--- internal/cmds/cmd.fmtx.go
+++ internal/cmds/cmd.fmtx.go
@@ -3,6 +3,7 @@ package cmds
 import (
 	"fmt"
 	"github.com/apex/log"
+	"github.com/cheggaaa/pb/v3"
 	"github.com/darmiel/gohtml"
 	"github.com/urfave/cli/v2"
 	"io/fs"
@@ -20,23 +21,45 @@ var CmdFormat = &cli.Command{
 		&cli.BoolFlag{
 			Name:    "dry-run",
 			Aliases: []string{"n"},
+			Usage:   "do not actually write files",
 		},
 		&cli.StringFlag{
 			Name:  "ignore-pattern",
 			Value: "(.*)\\.idea(.*)",
+			Usage: "ignore files matching regex pattern",
 		},
 		&cli.BoolFlag{
 			Name:    "no-backup",
 			Value:   false,
 			Aliases: []string{"B"},
+			Usage:   "do not create backup files before writing",
+		},
+		&cli.BoolFlag{
+			Name:    "remove-empty-lines",
+			Value:   false,
+			Aliases: []string{"L"},
+			Usage:   "remove empty lines in xml files",
+		},
+		&cli.BoolFlag{
+			Name:    "fast-fail",
+			Value:   false,
+			Aliases: []string{"x"},
+			Usage:   "exit if any file fails to format",
 		},
 	},
 	Action: func(ctx *cli.Context) error {
 		log.Info("Looking for *.xml files and formatting them ...")
-		var dry = ctx.Bool("dry-run")
-		var ignore = regexp.MustCompile(ctx.String("ignore-pattern"))
-		var backup = !ctx.Bool("no-backup")
 
+		var (
+			dry      = ctx.Bool("dry-run")
+			backup   = !ctx.Bool("no-backup")
+			failFast = ctx.Bool("fast-fail")
+			ignore   = regexp.MustCompile(ctx.String("ignore-pattern"))
+		)
+
+		var files []string
+
+		// collect files to process
 		if err := filepath.WalkDir(".", func(path string, info fs.DirEntry, _ error) error {
 			if strings.ToLower(filepath.Ext(path)) != ".xml" {
 				return nil
@@ -45,14 +68,30 @@ var CmdFormat = &cli.Command{
 				log.Infof("Ignoring file: %s", path)
 				return nil
 			}
-			log.Infof("Formatting file: %s", path)
+			files = append(files, path)
+			return nil
+		}); err != nil {
+			return err
+		}
+
+		// process files
+		//tpl := `{{ red "Formatting:" }} {{string . "path" | green}} {{ bar . "<" "-" (cycle . "↖" "↗" "↘" "↙" ) "." ">"}} {{speed . | rndcolor }} {{percent .}} `
+		//bar := pb.ProgressBarTemplate(tpl).Start(len(files))
+		bar := pb.Full.Start(len(files))
+		for _, path := range files {
+			bar.Increment()
+			bar.Set("prefix", "Formatting: "+path+" | ")
+
 			var (
 				err  error
 				data []byte
 			)
 			if data, err = os.ReadFile(path); err != nil {
 				log.WithError(err).Warnf("Cannot read file '%s'", path)
-				return nil
+				if failFast {
+					return err
+				}
+				continue
 			}
 
 			// make backup?
@@ -68,29 +107,39 @@ var CmdFormat = &cli.Command{
 				}
 				if err = os.WriteFile(backFile, data, 0644); err != nil {
 					log.WithError(err).Errorf("Cannot create backup of '%s'", path)
-					return err
+					if failFast {
+						return err
+					}
+					continue
 				}
 			}
 
 			// update data
-			data = gohtml.FormatBytes(data, true)
+			data = gohtml.FormatBytes(data, !ctx.Bool("remove-empty-lines"))
 			if len(data) == 0 {
 				log.Warn("formatted data was 0 bytes.")
+				if failFast {
+					return err
+				}
+				continue
 			}
-
-			log.Infof("Writing to '%s':", path)
-			fmt.Println(string(data))
 
 			if !dry {
 				var f *os.File
 				if f, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755); err != nil {
 					log.WithError(err).Warnf("Cannot open '%s'", path)
-					return nil
+					if failFast {
+						return err
+					}
+					continue
 				}
 
 				if err = f.Truncate(0); err != nil {
 					log.WithError(err).Warnf("Cannot truncate '%s'", path)
-					return nil
+					if failFast {
+						return err
+					}
+					continue
 				}
 
 				_, err = f.Seek(0, 0)
@@ -99,13 +148,18 @@ var CmdFormat = &cli.Command{
 				// close
 				if err = f.Close(); err != nil {
 					log.WithError(err).Warnf("Cannot close '%s'", path)
-					return nil
+					if failFast {
+						return err
+					}
+					continue
 				}
+			} else {
+				log.Infof("[dry-run] %s would now look like this:", path)
+				fmt.Println(string(data))
 			}
-			return nil
-		}); err != nil {
-			return err
 		}
+		bar.Finish()
+
 		return nil
 	},
 }
